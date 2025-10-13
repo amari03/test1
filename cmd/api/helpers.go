101e1be5faedc502dd1e61a688600a4b3d155e2f
Addie Vasquez <2020151434@ub.edu.bz>
2025-10-13 12:12:53 -0600

    setting up the database

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"strconv"
+	"net/http"
 	"errors"
 	"fmt"
 	"io"
-	"net/http"
-	"strconv"
 	"strings"
+	//"net/url"
+	//"github.com/amari03/test1/internal/validator"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -87,38 +89,4 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst int
 	}
 
 	return nil
-}
-
-// Standardized error responses
-func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message interface{}) {
-	env := envelope{"error": message}
-	err := app.writeJSON(w, status, env, nil)
-	if err != nil {
-		app.logger.Error(err.Error())
-		w.WriteHeader(500)
-	}
-}
-
-func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
-	app.logger.Error(err.Error())
-	message := "the server encountered a problem and could not process your request"
-	app.errorResponse(w, r, http.StatusInternalServerError, message)
-}
-
-func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
-	message := "the requested resource could not be found"
-	app.errorResponse(w, r, http.StatusNotFound, message)
-}
-
-func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
-	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
-	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
-}
-
-func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
-	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
-}
-
-func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
-	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
 }
